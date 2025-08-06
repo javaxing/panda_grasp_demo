66a6e131f36a677e6b9f4ec772398d8b57d8957d
xingxiaochi <admin@example.com>
2025-08-06 15:41:47 +0800

    1

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'panda_grasp_demo'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer='yourname',
+    maintainer_email='you@example.com',
+    description='Auto grasp demo with MoveIt 2',
+    license='MIT',
     entry_points={
         'console_scripts': [
+            'auto_grasp = panda_grasp_demo.auto_grasp:main',
         ],
     },
 )
